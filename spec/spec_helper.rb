578a1f201e8833b6431de16700b8f1bfb9ac8521
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-04-04 10:44:30 +0200

    Removed dependency on rspec_on_rails RailsStory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'spec'
 require 'spec/mocks'
 require 'spec/story'
-require 'spec/rails/story_adapter'
+
 
 dir = File.dirname(__FILE__)
 Dir[File.expand_path("#{dir}/../lib/*.rb")].uniq.each do |file|
@@ -11,3 +11,8 @@ end
 
 require File.expand_path(File.dirname(__FILE__) + "/temp/step_speccer_spec_step.rb")
 
+
+
+class RailsStory
+  
+end
