72e983911326ecea9d98072344c230cfc45fd720
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-04-05 17:53:04 +0200

    Fixing example spec.

--- generators/stepspecr/templates/example_step_spec.rb
+++ generators/stepspecr/templates/example_step_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/stepspecr_helper.rb")
 describe "Given $count $models" do 
   it "should create the specified model" do
     StepSpecr.run do
-      steps_for :resources
+      steps_for :example
       initial   "class SpecificModel;end; SpecificModel.should_receive(:create)"
       step      "Given 1 specific_model"
     end
