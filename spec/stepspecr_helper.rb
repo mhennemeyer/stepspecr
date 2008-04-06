fc6419597a7d492f432f8f7ea18f5976d8346a4c
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-04-06 17:57:01 +0200

    Rewrite the whole thing.

--- spec/stepspecr_helper.rb
+++ spec/stepspecr_helper.rb
@@ -8,13 +8,3 @@ dir = File.dirname(__FILE__)
 Dir[File.expand_path("#{dir}/../lib/*.rb")].uniq.each do |file|
   require file
 end
-
-require File.expand_path(File.dirname(__FILE__) + "/temp/step_speccer_spec_step.rb")
-
-class RailsStory
-  
-end
-
-def reload_stepspecr
-  load File.dirname(__FILE__) + '/../lib/step_specr.rb'
-end
