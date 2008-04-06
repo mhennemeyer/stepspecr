8a2db3e55a55722a197426a99260425e605af45a
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-04-06 17:39:00 +0200

    Don't need temp directory and generators anymore.

--- generators/stepspecr/templates/example_step.rb
+++
@@ -1,6 +0,0 @@
-steps_for :example do
-  Given "$count $models" do |count,name|
-    klass = eval "#{name.singularize.camelize}"
-    count.to_i.times { klass.create }
-  end
-end
--- spec/temp/step_speccer_spec_step.rb
+++
@@ -1,4 +0,0 @@
-steps_for :step_specr_step do
-  Then "the step to be specd" do
-  end
-end
