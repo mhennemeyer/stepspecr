1f9f682c2a92bf20020adffb107e309d598b7cb0
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-04-21 13:40:29 +0200

    Tuning README.

--- generators/stepspecr/templates/example_step_spec.rb
+++ generators/stepspecr/templates/example_step_spec.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/example_steps.rb'
 describe "Given $count $models" do
   it "should create 1 article for count=1, models=article" do
     StepSpecr.spec "Given 1 article" do
-      
+      step_group :example
       before do
         class Article
         end
